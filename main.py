e66a2ed9edc3ebc08e140d1239f5ccba8f8282b1
wllandrew <andrew.willian@uel.br>
2025-06-12 21:56:16 -0300

    :sparkles: feat: adicionado funções para geração do tabuleiro

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-def game():
+import json
+import os
+import random
+
+"""
+Funções que lidam com o tabuleiro
+"""
+def tabuleiro() -> list[list]:
+    with open("config.json", 'r') as js:
+        config = json.load(js)
+        tamanho = config["Tabuleiro"]
+        return [["~"] * tamanho] * tamanho
+    
+def imprime_tabuleiro(tab : list[list]) -> None:
+    print("   ", end="")
+    for n in range(len(tab)):
+        print(f"{n + 1} ", end="")
+    print()
+    for n in range(len(tab)):
+        print(f" {chr(n + 65)} ", end="")
+        for m in range(len(tab[n])):
+            print(f"{tab[n][m]} ", end="")
+        print()
+
+"""
+Funções de configuração:
+"""
+def configura():
     pass
+
+def game():
+    tab = tabuleiro()
+    imprime_tabuleiro(tab)
 
 def main():
     game()
